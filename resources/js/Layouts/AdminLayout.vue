c161b909a8c21af7961a84ff87117dbf98ef2cc9
Agus <ariefromjava@gmail.com>
2024-02-25 13:11:17 +0700

    feat: admin page layout

---
+++ resources/js/Layouts/AdminLayout.vue
@@ -0,0 +1,39 @@
+<script setup>
+import Navbar from '@/Components/Admin/Navbar.vue'
+import Sidebar from '@/Components/Admin/Sidebar.vue'
+import { onMounted } from 'vue'
+import {
+  initAccordions,
+  initCarousels,
+  initCollapses,
+  initDials,
+  initDismisses,
+  initDrawers,
+  initDropdowns,
+  initModals,
+  initPopovers,
+  initTabs,
+  initTooltips
+} from 'flowbite'
+
+// initialize components based on data attribute selectors
+onMounted(() => {
+  initAccordions();
+  initCarousels();
+  initCollapses();
+  initDials();
+  initDismisses();
+  initDrawers();
+  initDropdowns();
+  initModals();
+  initPopovers();
+  initTabs();
+  initTooltips();
+})
+</script>
+
+<template>
+  <Navbar />
+  <Sidebar />
+  <slot />
+</template>
--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AdminController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -35,4 +36,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-require __DIR__.'/auth.php';
+Route::get('/admin', [AdminController::class, 'index']);
+
+require __DIR__ . '/auth.php';
